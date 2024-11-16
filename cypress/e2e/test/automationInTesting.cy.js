8bfeb7a8e954f21a6fdc04bb4d1a38ad97b022ef
Matías Sillen Ríos <matias.sillen@gmail.com>
2024-11-15 23:01:58 -0300

    Creación del caso de prueba: Verificar que el texto de la descripción del hotel sea el esperado

--- cypress/e2e/test/automationInTesting.cy.js
+++ cypress/e2e/test/automationInTesting.cy.js
@@ -20,4 +20,11 @@ describe("TEST", { testIsolation: false }, () => {
     cy.get('img[src="/images/rbp-logo.jpg"]').should("be.visible");
     cy.get('img[src="/images/room2.jpg"]').should("be.visible");
   });
+
+  it("Verificar que el texto de la descripción del hotel sea el esperado", () => {
+    cy.get(".col-sm-10 > p").should(
+      "have.text",
+      "Welcome to Shady Meadows, a delightful Bed & Breakfast nestled in the hills on Newingtonfordburyshire. A place so beautiful you will never want to leave. All our rooms have comfortable beds and we provide breakfast from the locally sourced supermarket. It is a delightful place."
+    );
+  });
 });
