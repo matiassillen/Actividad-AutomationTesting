2bac0301de0ebdd6f055a3bfac39fecbe17f019b
Matías Sillen Ríos <matias.sillen@gmail.com>
2024-11-15 21:52:40 -0300

    Creación de la carpeta test, el archivo automationInTesting y el caso de prueba: Verificar que la información del hotel esté presente en la página

---
+++ cypress/e2e/test/automationInTesting.cy.js
@@ -0,0 +1,18 @@
+describe("TEST", { testIsolation: false }, () => {
+  it("Verificar que la información del hotel esté presente en la página", () => {
+    cy.visit("https://automationintesting.online/");
+
+    cy.get("p").contains("Shady Meadows B&B").should("be.visible");
+    cy.get("p").find("span.fa.fa-home").should("be.visible");
+
+    cy.get("p")
+      .contains("The Old Farmhouse, Shady Street, Newfordburyshire, NE1 410S")
+      .should("be.visible");
+
+    cy.get("p").contains("012345678901").should("be.visible");
+    cy.get("p").find("span.fa.fa-phone").should("be.visible");
+
+    cy.get("p").contains("fake@fakeemail.com").should("be.visible");
+    cy.get("p").find("span.fa.fa-envelope").should("be.visible");
+  });
+});
